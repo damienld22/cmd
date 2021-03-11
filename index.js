4169a3c8f0ec404b13800a5eb881281b288d9cef
Damien Le Dantec <damien.ledantec@telecomsante.com>
2021-03-11 22:53:14 +0100

    Add get command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const minimist = require('minimist');
-const { displayError } = require('./lib/error');
+const { displayError } = require('./lib/output');
 const { saveCommand } = require('./lib/save');
+const { getCommand } = require('./lib/get');
 
 // Parse command line arguments
 const args = minimist(process.argv.slice(2))._;
@@ -13,6 +14,13 @@ if (args.length === 0) {
 switch (args[0]) {
   case 'save':
     saveCommand();
+    break;
+  case 'get':
+    if (!args[1]) {
+      displayError('The "get" command need keywords');
+      process.exit(1);
+    }
+    getCommand(args.slice(1));
     break;
   default:
     displayError(`"${args[0]}" is not a valid argument (save, search)`);
